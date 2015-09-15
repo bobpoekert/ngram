24ebabe7c153f4e8e3e3335e4709e93a4599e24e
bob <bob@poekert.com>
2015-09-15 21:32:01 +0000

    actually write output to file

--- ngram_count_min.c
+++ ngram_count_min.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <stdint.h>
 #include <math.h>
+#include <inttypes.h>
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <sys/types.h>
@@ -63,10 +64,9 @@ uint32_t murmur3_32(const char *key, uint32_t len, uint32_t seed) {
 
 #define BIG_PRIME  9223372036854775783L
 #define MAX_LINE 131072
-#define MAX_LINE_WORDS 131072
 
-#define EPSILON 0.005
-#define DELTA 10E-7f
+#define W 54366
+#define D 24
 #define EXP_1 2.7182818284590451
 
 int main(int argc, char **argv) {
@@ -74,14 +74,14 @@ int main(int argc, char **argv) {
     unsigned char *sketch_buffer;
     int sketch_fd;
 
-    int max_line = MAX_LINE;
-    unsigned char input_line[MAX_LINE];
     int bytes_read;
 
-    int line_count = 0;
+    output_fname = argv[1];
+    
+    uint64_t line_count = 0;
 
     /* number of hash functions */
-    uint64_t d = (uint64_t) ceil(EXP_1 / EPSILON);
+    uint64_t d = D;
     size_t hash_array_width = d * 2;
 
     uint32_t hash_functions[hash_array_width];
@@ -94,10 +94,12 @@ int main(int argc, char **argv) {
     }
 
     /* width of array */
-    uint64_t w = (uint64_t) ceil(log(1 / DELTA));
+    uint64_t w = W;
+
+    printf("d: %" PRIu64 ", w: %" PRIu64 "\n", d, w);
 
     /* set up output file */
-    if ((sketch_fd = open(output_fname, O_RDWR)) == -1) {
+    if ((sketch_fd = open(output_fname, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)) == -1) {
         fprintf(stderr, "Failed to open output file: %s\n", output_fname);
         return 1;
     }
@@ -111,8 +113,15 @@ int main(int argc, char **argv) {
     /* allocate output file size */
     ftruncate(sketch_fd, file_size);
 
-    sketch_buffer = mmap(0, file_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, sketch_fd, 0);
+    printf("calling mmap; size: %d\n", file_size);
+    sketch_buffer = mmap(0, file_size, PROT_READ | PROT_WRITE, MAP_SHARED, sketch_fd, 0);
 
+    if (sketch_buffer == MAP_FAILED) {
+        fprintf(stderr, "Failed to map file\n");
+        return 1;
+    }
+
+    printf("alocating pointers for d and w\n");
     uint64_t *file_d;
     uint64_t *file_w;
     file_d = (uint64_t *) sketch_buffer;
@@ -120,12 +129,14 @@ int main(int argc, char **argv) {
     file_w = (uint64_t *) sketch_buffer;
     sketch_buffer += sizeof(uint64_t);
 
+    printf("setting d and w\n");
     *file_d = d;
     *file_w = w;
 
     uint32_t *sketch_arrays;
     sketch_arrays = (uint32_t *) sketch_buffer;
 
+    printf("setting hash function params\n");
     /* write out hash function parameters */
     for (int i=0; i < d*2; i++) {
 
@@ -135,139 +146,160 @@ int main(int argc, char **argv) {
 
     sketch_arrays += sizeof(uint32_t) * (d * 2);
 
+    unsigned char word_buf[MAX_LINE];
+    size_t word_buf_end = 0;
+
+    printf("running\n");
+    int in_word = -1;
+    uint32_t prev_hash = 0;
     while(1) {
-        bytes_read = getline(&input_line, &max_line, stdin);
-        if (bytes_read < 1) break;
+        int inchar_int = getchar();
+        unsigned char inchar = (unsigned char) inchar_int;
 
-        int in_word = -1;
-        int word_start = 0;
-        int word_end = 0;
-        uint32_t prev_hash = 0;
-        
-        while (word_end < bytes_read) {
+        if (inchar_int == EOF) break;
+        if (word_buf_end >= MAX_LINE) word_buf_end = 0;
 
-            switch (input_line[word_end]) {
+        word_buf[word_buf_end] = inchar;
 
-                case 'a':
-                case 'b':
-                case 'c':
-                case 'd':
-                case 'e':
-                case 'f':
-                case 'g':
-                case 'h':
-                case 'i':
-                case 'j':
-                case 'k':
-                case 'l':
-                case 'm':
-                case 'n':
-                case 'o':
-                case 'p':
-                case 'q':
-                case 'r':
-                case 's':
-                case 't':
-                case 'u':
-                case 'v':
-                case 'w':
-                case 'x':
-                case 'y':
-                case 'z':
+        switch (inchar) {
 
-                case 'A':
-                case 'B':
-                case 'C':
-                case 'D':
-                case 'E':
-                case 'F':
-                case 'G':
-                case 'H':
-                case 'I':
-                case 'J':
-                case 'K':
-                case 'L':
-                case 'M':
-                case 'N':
-                case 'O':
-                case 'P':
-                case 'Q':
-                case 'R':
-                case 'S':
-                case 'T':
-                case 'U':
-                case 'V':
-                case 'W':
-                case 'X':
-                case 'Y':
-                case 'Z':
+            case 'a':
+            case 'b':
+            case 'c':
+            case 'd':
+            case 'e':
+            case 'f':
+            case 'g':
+            case 'h':
+            case 'i':
+            case 'j':
+            case 'k':
+            case 'l':
+            case 'm':
+            case 'n':
+            case 'o':
+            case 'p':
+            case 'q':
+            case 'r':
+            case 's':
+            case 't':
+            case 'u':
+            case 'v':
 
-                case '0':
-                case '1':
-                case '2':
-                case '3':
-                case '4':
-                case '5':
-                case '6':
-                case '7':
-                case '8':
-                case '9':
+            case 'x':
+            case 'y':
+            case 'z':
 
-                case '-':
+            case 'A':
+            case 'B':
+            case 'C':
+            case 'D':
+            case 'E':
+            case 'F':
+            case 'G':
+            case 'H':
+            case 'I':
+            case 'J':
+            case 'K':
+            case 'L':
+            case 'M':
+            case 'N':
+            case 'O':
+            case 'P':
+            case 'Q':
+            case 'R':
+            case 'S':
+            case 'T':
+            case 'U':
+            case 'V':
+            case 'W':
+            case 'X':
+            case 'Y':
+            case 'Z':
 
-                    /* This is part of a word */
-                    if (in_word == 0) {
-                        /* We weren't in a word before,
-                         * so this is the start of a new word */
-                        word_start = word_end;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+
+            case '-':
+
+                /* This is part of a word */
+                in_word = 1;
+                break;
+            default:
+                /* This is not part of a word */
+                if (in_word == 1 && word_buf_end > 1) {
+                    /* we were in a word, and now we're not 
+                     * compute word hashes */
+                   /* for (int i=0; i < word_buf_end; i++) {
+                        putchar(word_buf[i]);
                     }
-                    in_word = 1;
+                    putchar('\n');*/
+                    //printf("hashing word length: %d\n", word_buf_end);
+                    uint32_t hash = murmur3_32(
+                            word_buf, word_buf_end-1, MURMUR_SEED);
+                    uint32_t bigram_hash = hash ^ prev_hash;
 
-                default:
-                    /* This is not part of a word */
-                    if (in_word == 1) {
-                        /* we were in a word, and now we're not 
-                         * compute word hashes */
-                        int word_length = word_end - word_start - 1;
-                        uint32_t hash = murmur3_32(
-                                input_line+word_start, word_length, MURMUR_SEED);
-                        uint32_t bigram_hash = hash ^ prev_hash;
+                    /* BEGIN SKETCH INC */
 
-                        /* BEGIN SKETCH INC */
+                   // printf("hashes: %d, %d\n", hash, bigram_hash);
 
-                        for (int i = 0; i < hash_array_width; i += 2) {
+                   // printf("inc sketch\n");
+                    int hash_idx = 0;
+                    for (int i = 0; i < hash_array_width-1; i += 2) {
 
-                            uint32_t a = hash_functions[i];
-                            uint32_t b = hash_functions[i+1];
+                        uint32_t a = hash_functions[i];
+                    //    printf("a: %d\n", a);
+                        uint32_t b = hash_functions[i+1];
+                    //    printf("b: %d\n", b);
 
-                            uint32_t column = (a * bigram_hash + b) % BIG_PRIME % w;
-                            uint32_t array_offset = w * i + column;
+                        uint32_t column = (a * bigram_hash + b) % BIG_PRIME % w;
+                        uint32_t array_offset = w * hash_idx + column;
 
-                            sketch_arrays[array_offset]++;
-                            
-                            column = (a * hash + b) % BIG_PRIME % w;
-                            array_offset = w * i + column;
-
-                            sketch_arrays[array_offset]++;
-
-                        }
+                        sketch_arrays[array_offset]++;
+                        /*printf("%d %d %d %d %d\n",
+                                sketch_arrays[array_offset], array_offset, w, hash_idx, column);*/
                         
+                        column = (a * hash + b) % BIG_PRIME % w;
+                        array_offset = w * hash_idx + column;
 
-                        /* END SKETCH INC */
+                        sketch_arrays[array_offset]++;
 
-                        prev_hash = hash;
+                      //  printf("hash idx: %d\n");
+                        hash_idx++;
+
                     }
-                    in_word = 0;
-            }
+                    
+                    /* END SKETCH INC */
 
-            word_end++;
+                    prev_hash = hash;
+                }
+                in_word = 0;
+
+                word_buf_end = 0;
+                if (inchar == '\n') {
+                    line_count++;
+                    if (line_count % 1000000 == 0) {
+                        printf("%" PRIu64 "\n", line_count);
+                    }
+                    if (line_count % 1100000000 == 0) {
+                        fsync(sketch_fd);
+                    }
+                    prev_hash = 0;
+                }
+                break;
         }
 
+        word_buf_end++;
+       // printf("%d\n", word_buf_end);
 
-        line_count++;
-        if (line_count % 10000 == 0) {
-            printf("%d\n", line_count);
-        }
     }
 
     return 0;
